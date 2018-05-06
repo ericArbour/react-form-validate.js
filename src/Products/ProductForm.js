178f77225ddf40db917c3f677a23a42a0cea7e7a
Eric Arbour <arboureric@gmail.com>
2018-05-06 17:40:22 -0400

    can bind submitted values to component state

--- src/Products/ProductForm.js
+++ src/Products/ProductForm.js
@@ -7,14 +7,17 @@ class ProductForm extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      values: {}
+    };
   }
 
   render() {
+    console.log(this.state.values);
     return (
       <div>
         <h2>Product Input Form</h2>
-        <Form>
+        <Form onSubmit={values => this.setState({ values })}>
           {formApi => (
             <form onSubmit={formApi.submitForm} id="form1" className="mb-4">
               <label htmlFor="name">Product Name</label>

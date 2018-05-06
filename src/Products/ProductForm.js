b370fddd4c0a12ff3ccc82983ed3bb4452ee549d
Eric Arbour <arboureric@gmail.com>
2018-05-06 16:41:52 -0400

    ProductForm component getting pulled into app.js

---
+++ src/Products/ProductForm.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ProductForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Some day I will be a form</h2>
+      </div>
+    );
+  }
+}
+
+export default ProductForm;
